b3e9f6f5a099980e6df09e7d43334b3fff9652e6
Badri A <111116985+badri-a@users.noreply.github.com>
2023-09-24 11:50:37 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Badri A\\development\\sdks\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Badri A\\Desktop\\compass" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Badri A\\development\\sdks\\flutter"
+  "PROJECT_DIR=C:\\Users\\Badri A\\Desktop\\compass"
+  "FLUTTER_ROOT=C:\\Users\\Badri A\\development\\sdks\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Badri A\\Desktop\\compass\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Badri A\\Desktop\\compass"
+  "FLUTTER_TARGET=C:\\Users\\Badri A\\Desktop\\compass\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC80NWY2ZTAwOTExMGRmNGYzNGVjMmNmOTlmNjNjZjczYjcxYjdhNDIwLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Badri A\\Desktop\\compass\\.dart_tool\\package_config.json"
+)
